9a446c828c28898dd2c53bf4d1b97f2cc8f8221d
thetanmaygupta <sardulgupta@gmail.com>
2021-06-20 12:18:09 -0700

    Update 2.0 !!!!

--- app/src/main/java/com/twayne/tactictoe/MainActivity3.kt
+++ app/src/main/java/com/twayne/tactictoe/MainActivity3.kt
@@ -1,24 +1,22 @@
 package com.twayne.tactictoe
 
 import android.content.Intent
-import android.media.MediaPlayer
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowInsets
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main3)
         /*Hide Status Bar And Action Bar Coz It Looks Weird With Splash*/
         getSupportActionBar()!!.hide();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
             window.insetsController?.hide(WindowInsets.Type.statusBars())
         Handler().postDelayed({
-            startActivity(Intent(this@MainActivity, MainActivity3::class.java))
+            startActivity(Intent(this@MainActivity3, MainActivity2::class.java))
             finish()
         }, 3000)
     }
